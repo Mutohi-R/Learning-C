2509cdb4a70dc7dd6d437db4d90e50e837eceb57
Mutohir <mutohirokuniyi@gmail.com>
2024-01-06 21:29:20 +0100

    2d arrays

---
+++ arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int data[2][3] = {{1, 2, 3}, {4, 5, 6}};
+
+    for (int i = 0; i < 2; i++)  {
+        for (int j = 0; j < 3; j++) {
+            printf("data[%d][%d] = ", i, j);
+            scanf("%d", &data[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            printf("data[%d][%d] = %d\n", i, j, data[i][j]);
+        }
+    }
+
+    return 0;    
+}
